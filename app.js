d1988137f1ab68814f9df449322aaed957798c47
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-09-07 15:41:18 +0300

    Add category creation form

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ require("dotenv").config();
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
+const categoriesRouter = require("./routes/categories");
 
 // setup mongoose connection
 mongoose.set("strictQuery", false);
@@ -37,6 +38,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/categories", categoriesRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -2,30 +2,31 @@ const asyncHandler = require("express-async-handler");
 const Category = require("../models/category");
 
 exports.viewAllCategories = asyncHandler(async (req, res, next) => {
-  res.send("NOT YET");
+  res.send("NOT YET,view1");
 });
 
 exports.viewCategory = asyncHandler(async (req, res, next) => {
-  res.send("NOT YET");
+  res.send("NOT YET,view2");
 });
 
 exports.createCategoryGet = asyncHandler(async (req, res, next) => {
-  res.send("NOT YET");
+  console.log("here");
+  res.render("createCategory");
 });
 exports.createCategoryPost = asyncHandler(async (req, res, next) => {
-  res.send("NOT YET");
+  res.send("NOT YET,view3");
 });
 
 exports.deleteCategoryGet = asyncHandler(async (req, res, next) => {
-  res.send("NOT YET");
+  res.send("NOT YET,view4");
 });
 exports.deleteCategoryPost = asyncHandler(async (req, res, next) => {
-  res.send("NOT YET");
+  res.send("NOT YET,view5");
 });
 
 exports.updateCategoryGet = asyncHandler(async (req, res, next) => {
-  res.send("NOT YET");
+  res.send("NOT YET,view6");
 });
 exports.updateCategoryPost = asyncHandler(async (req, res, next) => {
-  res.send("NOT YET");
+  res.send("NOT YET,view7");
 });

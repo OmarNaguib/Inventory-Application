15d14194efeb0c34a802d24a06e1a85ded67dfcf
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-09-07 10:27:53 +0300

    make dummy category controllers

---
+++ controllers/categoryController.js
@@ -0,0 +1,22 @@
+const asyncHandler = require("express-async-handler");
+const Category = require("../models/category");
+
+exports.viewAllCategories = asyncHandler(async (req, res, next) => {
+  res.send("NOT YET");
+});
+
+exports.viewCategory = asyncHandler(async (req, res, next) => {
+  res.send("NOT YET");
+});
+
+exports.createCategory = asyncHandler(async (req, res, next) => {
+  res.send("NOT YET");
+});
+
+exports.deleteCategory = asyncHandler(async (req, res, next) => {
+  res.send("NOT YET");
+});
+
+exports.updateCategory = asyncHandler(async (req, res, next) => {
+  res.send("NOT YET");
+});

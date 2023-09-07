ff87e81fd6e87e04ad3e8d3091f3891f8efc4109
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-09-07 10:28:28 +0300

    Make dummy item controllers

---
+++ controllers/itemController.js
@@ -0,0 +1,22 @@
+const asyncHandler = require("express-async-handler");
+const Item = require("../models/item");
+
+exports.viewAllCategories = asyncHandler(async (req, res, next) => {
+  res.send("NOT YET");
+});
+
+exports.viewItem = asyncHandler(async (req, res, next) => {
+  res.send("NOT YET");
+});
+
+exports.createItem = asyncHandler(async (req, res, next) => {
+  res.send("NOT YET");
+});
+
+exports.deleteItem = asyncHandler(async (req, res, next) => {
+  res.send("NOT YET");
+});
+
+exports.updateItem = asyncHandler(async (req, res, next) => {
+  res.send("NOT YET");
+});

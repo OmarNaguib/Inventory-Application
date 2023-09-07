d5b4f41bf25665c00a547a9da5a788ed46e0d496
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-09-07 06:52:40 +0300

    Fix layout path

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(expressLayouts);
-app.set("layout", "./views/layout");
+app.set("layout", "./layout.ejs");
 
 app.use(logger("dev"));
 app.use(express.json());

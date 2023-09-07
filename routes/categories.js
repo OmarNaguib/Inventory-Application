ef30a0d64cdddaefc6bad12562afcd6000503d6e
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-09-07 15:18:15 +0300

    Add controller routes

---
+++ routes/categories.js
@@ -0,0 +1,19 @@
+const { Router } = require("express");
+
+const router = Router();
+
+const categoryController = require("../controllers/categoryController");
+
+router.get("/", categoryController.viewAllCategories);
+router.get("/:id", categoryController.viewCategory);
+
+router.get("/create".categoryController.createCategoryGet);
+router.post("/create".categoryController.createCategoryPost);
+
+router.get("/:id/delete".categoryController.deleteCategoryGet);
+router.post("/:id/delete".categoryController.deleteCategoryPost);
+
+router.get("/:id/update".categoryController.updateCategoryGet);
+router.post("/:id/update".categoryController.updateCategoryPost);
+
+module.exports = router;

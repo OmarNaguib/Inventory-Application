11c18b65f3d4a7ef6fc7312192d7d0f3f37be8fc
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-09-07 15:19:38 +0300

    Add item routes

---
+++ routes/items.js
@@ -0,0 +1,19 @@
+const { Router } = require("express");
+
+const router = Router();
+
+const itemController = require("../controllers/itemController");
+
+router.get("/", itemController.viewAllCategories);
+router.get("/:id", itemController.viewItem);
+
+router.get("/create".itemController.createItemGet);
+router.post("/create".itemController.createItemPost);
+
+router.get("/:id/delete".itemController.deleteItemGet);
+router.post("/:id/delete".itemController.deleteItemPost);
+
+router.get("/:id/update".itemController.updateItemGet);
+router.post("/:id/update".itemController.updateItemPost);
+
+module.exports = router;

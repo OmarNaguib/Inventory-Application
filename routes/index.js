e71674d0c47a62f4d23842231fd0128b42b11ff5
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-09-11 19:02:01 +0300

    Make index controller

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,28 @@
 const express = require("express");
+const asyncHandler = require("express-async-handler");
+const Category = require("../models/category");
+const Item = require("../models/item");
 
 const router = express.Router();
 
 /* GET home page. */
-router.get("/", (req, res, next) => {
-  res.render("index", { title: "Express" });
-});
+router.get(
+  "/",
+  asyncHandler(async (req, res, next) => {
+    const itemsByCategory = await Item.aggregate([
+      {
+        $group: { _id: "$category", items: { $push: "$$ROOT" } },
+      },
+    ]).exec();
+    const itemsByCategoryName = await Category.populate(itemsByCategory, {
+      path: "items.category",
+    });
+
+    res.render("index", {
+      title: "Inventory Application",
+      itemsByCategoryName,
+    });
+  })
+);
 
 module.exports = router;
